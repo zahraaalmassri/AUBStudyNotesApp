395bb008fedc67e904d376c97a27d05e1a350373
zahraamaa <zahraama538@gmail.com>
2026-05-16 01:55:21 +0300

    Refined AUB themed UI AND improved course organization

--- app/src/main/java/com/example/aubstudynotesapp/Course.java
+++ app/src/main/java/com/example/aubstudynotesapp/Course.java
@@ -3,12 +3,18 @@ package com.example.aubstudynotesapp;
 public class Course {
 
     String name;
+    String semester;
 
-    public Course(String name) {
+    public Course(String name, String semester) {
         this.name = name;
+        this.semester = semester;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getSemester() {
+        return semester;
     }
 }
--- app/src/main/java/com/example/aubstudynotesapp/CoursesActivity.java
+++ app/src/main/java/com/example/aubstudynotesapp/CoursesActivity.java
@@ -1,6 +1,9 @@
 package com.example.aubstudynotesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,6 +19,8 @@ public class CoursesActivity extends AppCompatActivity {
 
     CourseAdapter adapter;
 
+    Button btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +28,29 @@ public class CoursesActivity extends AppCompatActivity {
 
         recyclerCourses = findViewById(R.id.recyclerCourses);
 
+        btnLogout = findViewById(R.id.btnLogout);
+
         courseList = new ArrayList<>();
 
-        courseList.add(new Course("CMPS 297"));
-        courseList.add(new Course("Mobile Development"));
-        courseList.add(new Course("Artificial Intelligence"));
-        courseList.add(new Course("Database Systems"));
-        courseList.add(new Course("Operating Systems"));
+        courseList.add(new Course(
+                "CMPS 279 - Mobile App Development",
+                "Spring 2026"));
+
+        courseList.add(new Course(
+                "CMPS 285 - Artificial Intelligence",
+                "Spring 2026"));
+
+        courseList.add(new Course(
+                "CMPS 231 - Data Structures",
+                "Fall 2025"));
+
+        courseList.add(new Course(
+                "MATH 251 - Linear Algebra",
+                "Spring 2026"));
+
+        courseList.add(new Course(
+                "CMPS 212 - Database Systems",
+                "Fall 2025"));
 
         adapter = new CourseAdapter(courseList);
 
@@ -37,5 +58,19 @@ public class CoursesActivity extends AppCompatActivity {
                 new LinearLayoutManager(this));
 
         recyclerCourses.setAdapter(adapter);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent =
+                        new Intent(CoursesActivity.this,
+                                MainActivity.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        });
     }
 }
