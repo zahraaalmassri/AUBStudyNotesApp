632dbb20a1d7bf7536a1047693a3a3ad96209b75
zahraamaa <zahraama538@gmail.com>
2026-05-16 01:40:55 +0300

    Improved login validation and user authentication flow

--- app/src/main/java/com/example/aubstudynotesapp/MainActivity.java
+++ app/src/main/java/com/example/aubstudynotesapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aubstudynotesapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,16 +32,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String email =
-                        etEmail.getText().toString();
+                        etEmail.getText().toString().trim();
 
                 String password =
-                        etPassword.getText().toString();
+                        etPassword.getText().toString().trim();
 
                 if(email.isEmpty() || password.isEmpty()) {
 
                     Toast.makeText(
                             MainActivity.this,
-                            "Please enter email and password",
+                            "Please fill all fields",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
+                } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Enter valid email",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
+                } else if(password.length() < 6) {
+
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Password must be at least 6 characters",
                             Toast.LENGTH_SHORT
                     ).show();
 
